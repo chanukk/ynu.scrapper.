e8989dd0562410061941a155b0d5c22f39dcbef5
Your Name <you@example.com>
2024-08-26 16:48:39 +0900

    20240826 1648

--- scrapper.py
+++ scrapper.py
@@ -1,28 +1,72 @@
 import requests
 from bs4 import BeautifulSoup
 
-keyword = "간호사"
-response = requests.get(
-    f"https://search.incruit.com/list/search.asp?col=job&kw={keyword}&startno=0")
 
-soup = BeautifulSoup(response.text, "html.parser")
-lis = soup.find_all("li", class_="c_col")
+def search_incruit(keyword, page):
+    jobs = []
+    for i in range(page):
+        page = i * 30
+        response = requests.get(
+            f"https://search.incruit.com/list/search.asp?col=job&kw={keyword}&startno={page}")
 
-jobs = []
+        soup = BeautifulSoup(response.text, "html.parser")
+        lis = soup.find_all("li", class_="c_col")
 
-for li in lis:
-    company = li.find("a", class_="cpname").text
-    title = li.find("div", class_="cell_mid").find("a").text
-    location = li.find("div", class_="cell_mid").find_all("span")[3].text
-    link = li.find("div", class_="cell_mid").find("a").get("href")
 
-    job_data = {
-        "title" : title,
-        "company" : company,
-        "location" : location,
-        "link" : link
-    }
+        for li in lis:
+            company = li.find("a", class_="cpname").text
+            title = li.find("div", class_="cell_mid").find("a").text
+            location = li.find("div", class_="cell_mid").find_all("span")[3].text
+            link = li.find("div", class_="cell_mid").find("a").get("href")
 
-    jobs.append(job_data)
+            job_data = {
+                "title" : title,
+                "company" : company,
+                "location" : location,
+                "link" : link
+            }
 
-print(jobs)
+            jobs.append(job_data)
+        print(f"{i+1}번쨰 페이지를 스크롤링 완료했습니다.")
+    return jobs
+
+
+def search_jobkorea(keyword, page):
+    
+    jobs = []
+
+    for i in range(page):
+
+        page = i + 1
+
+        response = requests.get(
+            f"https://www.jobkorea.co.kr/Search/?stext={keyword}&tabType=recruit&Page_No={page}")
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        lis = soup.find_all("article", class_="list-item")
+
+
+        for li in lis:
+            try:
+                title = li.find("div", class_="information-title").find("a").text.strip()
+                company = li.find("div", class_="list-section-corp").find("a").text.strip()
+                location = li.find_all("li", class_="chip-information-item")[3].text
+                link = li.find("div", class_="information-title").find("a").get("href")
+                print(f"https://www.jobkorea.co.kr{link}")
+
+                job_data = {
+                    "title": title,
+                    "company": company,
+                    "location": location,
+                    "link": f"https://www.jobkorea.co.kr{link}"
+                }
+                
+                jobs.append(job_data)
+            except:
+                pass
+            
+            print(f"{i + 1}번째 페이지를 스크래핑 완료했습니다.")
+
+    return jobs
+
+
